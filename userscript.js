bebe3586e0b440ad4ae49753ba473ad2b792608c
LeonRay297 <123786505+LeonRay297@users.noreply.github.com>
2023-02-12 17:12:03 +0800

    更新 userscript.js

--- userscript.js
+++ userscript.js
@@ -1,18 +1,17 @@
 // ==UserScript==
-// @name                UEX——CN
-// @name:zh-CN          UEX汉化脚本
+// @name                UEX,CCUGame汉化脚本
+// @name:zh-CN          UEX,CCUGame汉化脚本
 // @namespace           https://github.com/CxJuice/Uex_Chinese_Translate
-// @version             0.9.4
-// @description         UEX——CN
-// @description:zh      UEX汉化插件
-// @description:zh-CN   UEX汉化插件
+// @version             0.9.6
+// @description         UEX_Chinese_Translate
+// @description:zh      UEX_Chinese
+// @description:zh-CN   UEX汉化
 // @author              CxJuice
-// @match               https://uexcorp.space/*
-// @match               https://www.erkul.games/*
 // @match               https://ccugame.app/*
+// @match               https://uexcorp.space/*
 // @grant               GM_xmlhttpRequest
 // @grant               GM_getResourceText
-// @resource            zh-CN https://cdn.jsdelivr.net/gh/CxJuice/Uex_Chinese_Translate@main/zh-CN-uex1.11.json
+// @resource            zh-CN https://cdn.jsdelivr.net/gh/CxJuice/Uex_Chinese_Translate@main/zh-CN-uex1.13.json
 // @require             https://cdn.bootcdn.net/ajax/libs/timeago.js/4.0.2/timeago.full.min.js
 // @require             https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
 // ==/UserScript==
@@ -20,26 +19,26 @@ window.addEventListener('load', function(event) {
   // 当页面加载完成时，执行我们的脚本
   myScript();
 });
-
+ 
 // 监听页面中的鼠标点击事件
 document.addEventListener('click', function(event) {
   // 当发生鼠标点击事件时，执行我们的脚本
   myScript();
 });
 function myScript() {
-
+ 
 (function() {
   'use strict';
-
+ 
   const SUPPORT_LANG = ["zh-CN", "ja"];
   const lang = (navigator.language || navigator.userLanguage);
   const locales = getLocales(lang)
-
+ 
   translateByCssSelector();
   translateDesc();
   traverseElement(document.body);
   watchUpdate();
-
+ 
   function getLocales(lang) {
     if(lang.startsWith("zh")) { // zh zh-TW --> zh-CN
       lang = "zh-CN";
@@ -52,12 +51,12 @@ function myScript() {
       dict: {}
     };
   }
-
+ 
   function translateRelativeTimeEl(el) {
     const datetime = $(el).attr('datetime');
     $(el).text(timeago.format(datetime, lang.replace('-', '_')));
   }
-
+ 
   function translateElement(el) {
     // Get the text field name
     let k;
@@ -70,32 +69,32 @@ function myScript() {
     } else {
       k = 'data';
     }
-
+ 
     const txtSrc = el[k].trim();
     const key = txtSrc.toLowerCase()
         .replace(/\xa0/g, ' ') // replace '&nbsp;'
         .replace(/\s{2,}/g, ' ');
-
+ 
     if(locales.dict[key]) {
       el[k] = el[k].replace(txtSrc, locales.dict[key])
     }
   }
-
+ 
   function shoudTranslateEl(el) {
     const blockIds = [];
     const blockClass = [
       "css-truncate" // 过滤文件目录
     ];
     const blockTags = [ "IMG", "svg"];
-
+ 
     if(blockTags.includes(el.tagName)) {
       return false;
     }
-
+ 
     if(el.id && blockIds.includes(el.id)) {
       return false;
     }
-
+ 
     if(el.classList) {
       for(let clazz of blockClass) {
         if(el.classList.contains(clazz)) {
@@ -103,21 +102,21 @@ function myScript() {
         }
       }
     }
-
+ 
     return true;
   }
-
+ 
   function traverseElement(el) {
     if(!shoudTranslateEl(el)) {
       return
     }
-
+ 
     for(const child of el.childNodes) {
       if(["RELATIVE-TIME", "TIME-AGO"].includes(el.tagName)) {
         translateRelativeTimeEl(el);
         return;
       }
-
+ 
       if(child.nodeType === Node.TEXT_NODE) {
         translateElement(child);
       }
@@ -132,7 +131,7 @@ function myScript() {
       }
     }
   }
-
+ 
   function watchUpdate() {
     const m = window.MutationObserver || window.WebKitMutationObserver;
     const observer = new m(function (mutations, observer) {
@@ -142,14 +141,14 @@ function myScript() {
         }
       }
     });
-
+ 
     observer.observe(document.body, {
       subtree: true,
       characterData: true,
       childList: true,
     });
   }
-
+ 
   // translate "about"
   function translateDesc() {
     $(".repository-content .f4").append("<br/>");
@@ -163,11 +162,11 @@ function myScript() {
         .end()
         .text()
         .trim();
-
+ 
       if(!desc) {
         return;
       }
-
+ 
       GM_xmlhttpRequest({
         onload: function(res) {
           if (res.status === 200) {
@@ -184,7 +183,7 @@ function myScript() {
       });
     });
   }
-
+ 
   function translateByCssSelector() {
     if(locales.css) {
       for(var css of locales.css) {
